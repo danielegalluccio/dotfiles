2537386efb9203e3e053fea910dcdb54386072c9
Daniele Galluccio <daniele.galluccio@gmail.com>
2020-06-25 15:39:56 +0100

    autoinstall homebrew

--- brew.sh
+++ brew.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
-brew update
+which -s brew
+if [[ $? != 0 ]] ; then
+	# Install homebrew
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    brew update
+    # Upgrade any already-installed formulae.
+    brew upgrade
+fi
 
-# Upgrade any already-installed formulae.
-brew upgrade
 
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
